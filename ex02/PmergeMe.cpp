37e9a191d05a5a8141a8e466fadae422b948f097
akrepkov <115089321+akrepkov@users.noreply.github.com>
2024-11-13 16:12:53 +0000

    Start vector sort

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <deque>
 #include <iostream>
+#include <algorithm>
+
 
 PmergeMe::PmergeMe(const PmergeMe& copy){
 //rewrite
@@ -11,23 +13,35 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& copy){
 }
 
 void PmergeMe::printVector(){
+    std::cout << "Vector" << std::endl;
     for(auto i= Vector.begin(); i != Vector.end(); ++i){
-        std::cout << " " << *i << " " << std::endl;
+        std::cout << " " << *i << " ";
     }
+    std::cout << std::endl;
 }
 
 void PmergeMe::printDeque(){
+    std::cout << "Deque" << std::endl;
     for(auto i= Deque.begin(); i != Deque.end(); ++i){
-        std::cout << " " << *i << " " << std::endl;
+        std::cout << " " << *i << " ";
     }
+    std::cout << std::endl;
 }
 
 
 void PmergeMe::sortVector()
 {
+
     printVector();
-
-
+    if (Vector.size()%2 == 0){
+        for(int i = 0; Vector.at(i); i += 2){
+            std::cout << "I " << i << " | ";
+            if (Vector[i] < Vector[i+1]){
+                std::swap(Vector[i],Vector[i+1]);
+            }
+        }
+    printVector();
+    }
 }
 
 void PmergeMe::sortDeque()
