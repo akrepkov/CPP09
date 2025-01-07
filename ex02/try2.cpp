73760bc2ac91f0703b23e6703be21c324d094d9d
akrepkov <115089321+akrepkov@users.noreply.github.com>
2025-01-07 13:40:49 +0000

    Vector is working

--- ex02/try2.cpp
+++ ex02/try2.cpp
@@ -14,7 +14,7 @@ bool isAscending(const std::vector<int>& numbers) {
 }
 
 
-std::vector<int> insertNumber(std::vector<int>& temp, int x, int size, int num) {
+int insertNumber(std::vector<int>& temp, int x, int size, int num) {
     // if (size > temp.size()) {
     //     std::cerr << "Error: Invalid size for erase operation.\n";
     //     return temp;
@@ -28,14 +28,12 @@ std::vector<int> insertNumber(std::vector<int>& temp, int x, int size, int num) 
 
     if (x < temp[0]) {
         temp.insert(temp.begin(), x); // Insert at the beginning
+		return 0;
     }
     else if (x > temp[size]) {
-		std::cout << "Should add at the end"<< std::endl;
+		//std::cout << "Should add at the end"<< std::endl;
         temp.insert(temp.begin() + size + 1, x); // Insert at the end
-		for (auto m = temp.begin(); m != temp.begin() + size; ++m) {
-			std::cout << " main  " << *m << " ";
-		}
-		std::cout << "\n";
+		return size + 1;
     }
     else {
         while (low <= high) {
@@ -47,10 +45,14 @@ std::vector<int> insertNumber(std::vector<int>& temp, int x, int size, int num) 
         }
         temp.insert(temp.begin() + low, x); // Insert at the correct position
     }
-    return temp;
+		// for (auto m = temp.begin(); m != temp.begin() + size + 1; ++m) {
+		// 	std::cout << " main  " << *m << " ";
+		// }
+		// std::cout << "\n";
+    return low;
 }
 
-std::vector<int> alghorithm(std::vector<int>& main) {
+std::vector<int> alghorithm(std::vector<int>& main, std::vector<int>& small) {
     std::vector<int> jacobsthal = {1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461};
     size_t j = 0;
     for (auto m = main.begin(); m != main.end(); ++m) {
@@ -63,21 +65,90 @@ std::vector<int> alghorithm(std::vector<int>& main) {
         size_t end = (j > 0) ? jacobsthal[j - 1] : 0;
 
         if (start > main.size() - 1 || end > start) break;
-        std::cout << "Start: " << start << ", End: " << end << std::endl;
+        // std::cout << "Start: " << start << ", End: " << end << std::endl;
 		int temp = start;
         while (start > end) {
-            std::cout << " insert " << main[temp] << "\n";
-            main = insertNumber(main, main[temp], end, temp);
+            // std::cout << " insert " << main[temp] << "\n";
+            int position = insertNumber(main, main[temp], end, temp);
+			if (small[start]){
+				int x = small[start];
+				small.erase(small.begin() + start);
+				small.insert(small.begin() + position, x);
+			}
             end++;
         }
         j++;
     }
+	std::cout << " SMALL  "  << " ";
+    for (auto m = small.begin(); m != small.end(); ++m) {
+        std::cout   << *m << " ";
+    }
 
+    std::cout << "\n";
+	std::cout << " BIG  "  << " ";
     for (auto m = main.begin(); m != main.end(); ++m) {
-        std::cout << " main  " << *m << " ";
+        std::cout   << *m << " ";
+    }
+	std::cout << "\n";
+    return main;
+}
+
+
+std::vector<int> insertSmall(std::vector<int>& temp, int x, int size) {
+    int low = 0;
+    int high = size;
+    int mid;
+
+    if (x < temp[0]) {
+        temp.insert(temp.begin(), x); // Insert at the beginning
+    }
+    else if (x > temp[size]) {
+		//std::cout << "Should add at the end"<< std::endl;
+        temp.insert(temp.begin() + size + 1, x); // Insert at the end
+    }
+    else {
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (x < temp[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        temp.insert(temp.begin() + low, x); // Insert at the correct position
+    }
+		for (auto m = temp.begin(); m != temp.begin() + size + 1; ++m) {
+			std::cout << " main  " << *m << " ";
+		}
+		std::cout << "\n";
+    return temp;
+}
+
+std::vector<int> algthjorithmforsmallpart(std::vector<int>& main, std::vector<int>& small) {
+	std::vector<int> jacobsthal = {1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461};
+	main.insert(main.begin(), small[0]);
+    size_t j = 0;
+	std::cout << "SMALL ";
+    for (auto m = small.begin(); m != small.end(); ++m) {
+        std::cout << *m << " ";
     }
     std::cout << "\n";
-    return main;
+	while (j < jacobsthal.size()) {
+        // Ensure start does not go out of bounds
+        size_t start = (jacobsthal[j] < small.size()) ? jacobsthal[j] : small.size() - 1;
+        size_t end = (j > 0) ? jacobsthal[j - 1] : 0;
+        if (start > main.size() - 1 || end >= start) break;
+        std::cout << "Start: " << start << ", End: " << end << std::endl;
+		int temp = start - end;
+        while (temp != 0) {
+			end +=2;
+            std::cout << " insert " << small[start] << "\n";
+            main = insertSmall(main, small[start], main.size() - 1);
+			start--;
+            temp--;
+        }
+        j++;
+    }
+	    return main;
 }
 
 int main(int argc, char **argv) {
@@ -118,8 +189,9 @@ int main(int argc, char **argv) {
     }
 
     // Apply the algorithm to the 'big' vector
-    big = alghorithm(big);
-
+    big = alghorithm(big, small);
+	
+	big = algthjorithmforsmallpart(big, small);
     std::cout << " result: \n";
     for (auto i = big.begin(); i != big.end(); ++i) {
         std::cout << *i << " ";
